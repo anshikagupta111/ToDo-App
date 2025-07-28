5cf9423715ead083bed04b809caa9db93c51bf4c
Anshika Gupta <anshikagupta93196639@gmail.com>
2025-07-28 15:46:30 +0530

    first commit

--- App.tsx
+++ App.tsx
@@ -5,24 +5,164 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+
+import { View ,TouchableOpacity,Text,Modal,TextInput,FlatList} from 'react-native';
+import React,{useState} from 'react';
+import styles from './Styles'
+// import {Divider} from 'react-native-paper'
+
+
+
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+  const[tasks, setTasks] = useState([]);
+  const[newTask,setNewTask]=useState('');
+  const[id,setId]=useState(0);
+  const[modalVisible, setModalVisible] = useState(false);
+  const [checked,setChecked]=useState(false);
+  const AddTask=()=> {
+    if(newTask!==""){
+      const taskItem={
+        id:id.toString(),
+        text:newTask,
+      };
+      setTasks(tasks.concat(taskItem))
+setNewTask("");
+setModalVisible(false)
+setId(id+1);
+    }
+   
+    
 
+
+  };
+  const deleteTask=(delid)=>{
+    setTasks(prevTask=>prevTask.filter(tasks=>tasks.id!==delid))
+   }
+   const toggleTask=(id)=>{
+    setTasks(prevTask=>prevTask.map(task=>task.id===id?{...task,checked:!task.checked}:task))
+// setTasks(prevTask=>prevTask.filter(tasks=>tasks.id!==id))
+   }
+   const categories=[
+    {id:'1',name:'Work',color:'#61DEA4'},
+    {id:'2',name:'Shopping',color:'#F45E6D'},
+    {id:'3',name:'Family',color:'#FFE761'},
+    {id:'4',name:'Personal',color:'#B678FF'}]
+
+
+    const itemCategory=()=>{
+      
+    }
   return (
     <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+    <View style={styles.header}>
+      <Text style={styles.headercontent}>Today</Text>
+       <TouchableOpacity style={styles.circle} onPress={()=>setModalVisible(true)}>
+        <Text style={styles.plus}>+</Text>
+       </TouchableOpacity>
     </View>
-  );
+
+<View>
+  
+
+     <FlatList
+     style={styles.flatlist}
+        data={tasks}
+        
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+         
+          <View style={styles.list} >
+           
+            <View style={styles.checkbox} >
+              <TouchableOpacity style={styles.clickcheck} 
+              onPress={()=>toggleTask(item.id)}
+              onLongPress={()=>deleteTask(item.id)}
+              >
+                
+                <Text>{item.checked?"✓":""}</Text>
+                
+              </TouchableOpacity>
+            </View>
+            
+            <Text style={styles.tasklist}>{item.text}</Text>
+            <View style={[styles.colorButton , {backgroundColor:item.color}]}></View>
+
+
+
+          </View>
+          
+          
+           
+        )}
+        
+      
+       ItemSeparatorComponent={() => (
+    <View style={styles.line} />
+  )}
+      />
+      
+
+      </View>
+
+
+    <Modal
+    // animationType="slide"
+    animationType="fade"
+    transparent={true}
+    // backdropColor='white'
+    visible={modalVisible}
+    onRequestClose={()=>setModalVisible(false)} 
+    >
+      <View style={styles.modalContainer}>
+        <View>
+        <Text style={styles.todo}>Add Todo</Text>
+        <TextInput
+        style={styles.textInput}
+        placeholder='Enter your task'
+        value={newTask}
+        onChangeText={setNewTask}
+        
+        />
+        </View >
+        <View style={styles.donebutton}>
+          <TouchableOpacity  style={styles.cancel}onPress={()=>setModalVisible(false)}>
+            <Text style={styles.canceltext}>Cancel</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={AddTask} style={styles.done}>
+            <Text style={styles.donetext}>Done</Text>
+          </TouchableOpacity>
+            
+        </View>
+              </View>
+      
+
+    </Modal>
+    <View >
+      
+    </View>
+    <FlatList
+    data={categories}
+    keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <TouchableOpacity 
+          style={[styles.categoryCard,{backgroundColor:item.color}]}
+          onPress={()=>itemCategory()}
+          >
+             <Text style={styles.categoryName}>{item.name}</Text>
+      {/* <Text style={styles.taskCount}>{item.tasks} </Text> */}
+          </TouchableOpacity>
+
+
+        )}
+    />
+   
+</View>
+
+   
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+
 
 export default App;
---
+++ Styles.tsx
@@ -0,0 +1,172 @@
+import { StyleSheet } from "react-native"
+export default StyleSheet.create({
+  modalContainer:{
+    width:340.62,
+    height:297.24,
+    marginTop:128.76,
+    marginLeft:17.13,
+    borderRadius:10,
+    borderWidth:1,
+    borderColor:'#EBEFF5',
+    backgroundColor: 'white'
+    
+  },
+  colorButton:{
+    height:15,
+    width:15,
+    borderWidth:1,
+    borderRadius:20,
+    // marginRight:10
+  },
+  categoryCard:{
+    height:80,
+    borderRadius:15,
+    justifyContent:'center',
+    // marginLeft:60,
+    marginBottom:8
+  },
+  categoryName:{
+    fontWeight:'bold',
+    color:'white',
+    marginLeft:30
+  },
+ tasklist:{
+//   marginLeft:40
+// marginRight:100
+flex:1,
+marginLeft:10
+ },
+  line:{
+    height:1,
+  
+    backgroundColor:'grey'
+  },
+  // flatlist:{
+  // justifyContent:'space-between'
+  // },
+  donebutton:{
+    height:44,
+    width:298,
+    flexDirection:'row',
+    marginTop:10,
+    marginLeft:33
+  },
+  textInput:{
+    width:298,
+    height:148,
+    marginTop:14,
+    marginLeft:20,
+   borderRadius:10,
+    borderWidth:2,
+    borderColor:"#EBEFF5"
+  },
+  list:{
+      //  backgroundColor: '#61DEA4',
+    padding: 15,
+    marginBottom: 10,
+    borderRadius: 10,
+    
+    justifyContent:'space-between',
+    flexDirection:'row',
+    height:55
+
+
+  },
+  checkbox:{
+    borderRadius:20,
+    backgroundColor:'white',
+    height:30,
+    width:30,
+    justifyContent:'center',
+    alignItems:'center',
+    borderWidth:1,
+    borderColor:'grey',
+    flexDirection:'row',
+    alignContent:'center'
+
+  },
+  todo:{
+    width:107,
+    height:41,
+    marginTop:10,
+    marginLeft:30,
+    fontWeight:700,
+    fontSize:18,
+    lineHeight:41,
+    letterSpacing:1
+  },
+  cancel:{
+    // flexDirection:'row',
+    width:86,
+    height:40,
+    marginTop:11,
+    marginLeft:12.71,
+    
+    
+  },
+  
+  done:{
+     width:86,
+    height:40,
+    marginTop:11,
+    // position:
+    marginLeft:123.53,
+  },
+  donetext:{
+    fontWeight:600,
+    fontSize:18,
+    letterSpacing:-0.43,
+    color:"#006CFF"
+  },
+  canceltext:{
+    color:"#006CFF",
+    fontWeight:400,
+    fontSize:18,
+    letterSpacing:-0.43,
+  },
+
+  container:{
+    flex:1,
+     paddingTop: 60,
+      paddingHorizontal: 20 ,
+      backgroundColor: '#FFF', 
+      
+      // paddingTop: 60,
+      //  paddingHorizontal: 20
+  },
+  header:{
+    flexDirection:'row',
+    // height:73,
+    // width: 353,
+    justifyContent: 'space-between', alignItems: 'center', marginBottom: 30
+    // justifyContent: 'center',
+    // alignItems: 'center',
+  },
+  circle:{
+    width: 40,
+    height: 40,
+    borderRadius: 20,               // Circle shape
+    borderWidth: 2,
+    borderColor: '#006CFF',         // Blue border
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  plus:{
+ fontSize: 24,
+    color: '#006CFF',               // Blue +
+    lineHeight: 26,
+    fontWeight: '600',
+    marginTop: -2, 
+
+  },
+  headercontent:{
+    fontSize:32,
+    letterSpacing:1,
+    fontWeight:700,
+    lineHeight:41,
+  
+
+
+
+  }
+})
