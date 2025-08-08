0052ab53b8e50d3177f312e2f2cc485afdbc0000
Anshika Gupta <anshikagupta93196639@gmail.com>
2025-08-08 18:47:25 +0530

    edit all task file

--- app/screens/AllTasks/AllTasks.tsx
+++ app/screens/AllTasks/AllTasks.tsx
@@ -4,20 +4,20 @@ import { View,TouchableOpacity,Text } from "react-native";
 import { FlatList } from "react-native";
 import styles from "../../../Styles";
 
-const AllTasks=({ route, onToggle, onEdit, onDelete })=>{
+const AllTasks=({ route, toggleTask, editTask , deleteTask })=>{
     const {tasks}=route.params
      const renderItem = ({ item }) => (
     <View style={styles.taskItem}>
-      <TouchableOpacity onPress={() => onToggle(item.id)}>
+      <TouchableOpacity onPress={() => toggleTask(item.id)}>
         <Text>
           {item.text}
         </Text>
       </TouchableOpacity>
       <View style={styles.buttonItem}>
-        <TouchableOpacity onPress={() => onEdit(item.id)}>
+        <TouchableOpacity onPress={() => editTask (item.id)}>
           <Text >Edit</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => onDelete(item.id)}>
+        <TouchableOpacity onPress={() =>deleteTask(item.id)}>
           <Text>Delete</Text>
         </TouchableOpacity>
       </View>
